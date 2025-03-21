63607a6b375cc3c36efd5b447f9d37eb1534f1da
HutchAlpha <qboisset@hotmail.com>
2025-03-21 15:57:13 +0100

    Ajout des fichiers du proje

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,11 @@
 //Mettre le code JavaScript lié à la page photographer.html
+async function TraitementPhotographer() {
+    const { name, portrait } = data;
+
+    const photographerDonnee = `data/${name}`;
+
+
+    for (let i = 0; i < photographerDonnee.length; i++) {
+
+    }
+}
